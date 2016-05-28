999baf37c4b06356230fd5ea592a4e48cd5ddc14
etika <etikaahuja@gmail.com>
2016-05-28 17:32:24 +0530

    Added ad sense

--- config/initializers/ads.rb
+++ config/initializers/ads.rb
@@ -1,10 +1,10 @@
 Ads.configure do |config|
-# config.renderer = lambda { |options|
-#     tag(
-#       :img,
-#       src: "http://placehold.it/#{options[:width]}x#{options[:height]}&text=Adsense"
-#     )
-#   }
-    config.renderer = nil
+config.renderer = lambda { |options|
+    tag(
+      :img,
+      src: "http://placehold.it/#{options[:width]}x#{options[:height]}&text=Adsense"
+    )
+  }
+    # config.renderer = nil
 
 end
