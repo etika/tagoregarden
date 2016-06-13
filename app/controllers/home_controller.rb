d0faaef0dd5e809b24ba0b84f312ae425427d1af
etika <etikaahuja@gmail.com>
2016-06-13 23:57:47 +0530

    Adding seo tags

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,8 @@ class HomeController < ApplicationController
   def index
   end
   def coming_soon
+     set_meta_tags title: "Tagore Garden Wala",
+                keywords: "tagore garden wala,tagore garden,tagore",
+                description: "Coming Soon,tagore garden wala,tagore garden,tagore"
   end
 end
