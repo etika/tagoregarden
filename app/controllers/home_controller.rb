4be235b4ff5ef6c99a178cbfdae7c3a05a85bad4
etika <etikaahuja@gmail.com>
2016-06-14 00:05:13 +0530

    Added new keywords

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
   end
   def coming_soon
      set_meta_tags title: "Tagore Garden Wala",
-                keywords: "tagore garden wala,tagore garden,tagore",
-                description: "Coming Soon,tagore garden wala,tagore garden,tagore"
+                keywords: "tagore garden wala,tagore garden,tagore,etikaz",
+                description: "Coming Soon,tagore garden wala,tagore garden,tagore,etikaz"
   end
 end
