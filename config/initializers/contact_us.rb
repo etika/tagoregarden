0479e76eb3623f3ef1b0cce72c2d70f76d64027a
etika <etikaahuja@gmail.com>
2016-07-04 15:16:14 +0530

    changes in contact_us

--- config/initializers/contact_us.rb
+++ config/initializers/contact_us.rb
@@ -6,7 +6,7 @@ ContactUs.setup do |config|
   # Configure the e-mail address which email notifications should be sent from.  If emails must be sent from a verified email address you may set it here.
   # Example:
   # config.mailer_from = "contact@please-change-me.com"
-  config.mailer_from = nil
+  # config.mailer_from = nil
 
   # Configure the e-mail address which should receive the contact form email notifications.
   config.mailer_to = "tagoregardenwala@gmail.com"
@@ -25,6 +25,6 @@ ContactUs.setup do |config|
   config.form_gem = 'simple_form'
 
   # Configure the redirect URL after a successful submission
-  config.success_redirect = '/contact-us'
+  config.success_redirect = '/'
 
 end
