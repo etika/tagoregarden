f90e341b761bb49e090df0f46cf5fa8feb050f61
etika <etikaahuja@gmail.com>
2016-08-02 19:16:41 +0530

    Added read permission on s3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,7 +97,11 @@ Rails.application.configure do
     access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
     secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
     s3_region: ENV.fetch('AWS_REGION'),
+   :s3_permissions => 'public-read'                  
+
   }
 }
+Paperclip.options[:command_path] = "/usr/local/bin/"
+
 
 end
