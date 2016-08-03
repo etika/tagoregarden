95fbe8426fd042fd25a0f5e4b1a1b30a4b2ad8cd
etika <etikaahuja@gmail.com>
2016-08-03 17:59:27 +0530

    Changes in paperclip and footer

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,4 +1,4 @@
 Paperclip::Attachment.default_options[:url] = 'tagoregardenwala-images.s3-website.ap-south-1.amazonaws.com/'
 Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
-Paperclip::Attachment.default_options[:s3_host_name] = 's3-website.ap-south-1.amazonaws.com/'
+Paperclip::Attachment.default_options[:s3_host_name] = 's3.ap-south-1.amazonaws.com/'
  
