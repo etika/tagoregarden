c0b1a1bf619074f2d968c132ea938af49ffaa4b0
etika <etikaahuja@gmail.com>
2016-08-10 18:34:31 +0530

    made changes in entries new page

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,17 +35,17 @@ Rails.application.configure do
   # Checks for improperly declared sprockets dependencies.
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
-Paperclip.options[:command_path] = "/usr/local/bin/"
-config.paperclip_defaults = {
-  storage: :s3,
-  s3_credentials: {
-    bucket: ENV.fetch('S3_BUCKET_NAME'),
-    access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
-    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-    s3_region: ENV.fetch('AWS_REGION'),
-   :s3_permissions => 'public-read'                  
-  }
-}
+# Paperclip.options[:command_path] = "/usr/local/bin/"
+# config.paperclip_defaults = {
+#   storage: :s3,
+#   s3_credentials: {
+#     bucket: ENV.fetch('S3_BUCKET_NAME'),
+#     access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+#     secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+#     s3_region: ENV.fetch('AWS_REGION'),
+#    :s3_permissions => 'public-read'                  
+#   }
+# }
 Paperclip.options[:command_path] = "/usr/local/bin/"
 
 
