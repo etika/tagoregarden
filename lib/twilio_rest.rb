25d6a39b9c69869228ce715699ae5af946872586
etika <etikaahuja@gmail.com>
2016-11-26 18:19:07 +0530

    changes

--- lib/twilio_rest.rb
+++ lib/twilio_rest.rb
@@ -5,7 +5,9 @@ def send_message(phone_number,activation_token)
   # @twilio_client = Twilio::REST::Client.new('ACa1c33e9559e054481e725e40442a4003', 'f618f30bc6511fbc79b93fe3e13cccb7')
   @twilio_client = Twilio::REST::Client.new('ACf85c4315f2e42d9c9ce3301343f01c5b', 'e2f8125f4b400649c1c7d40aa4028474')
 
- @twilio_client.messages.create(:from => '+15005550006',:to => number_to_send_to,:body =>"Security Code is kk")
+ # @twilio_client.messages.create(:from => '+15005550006',:to => number_to_send_to,:body =>"Security Code is kk")
+ @twilio_client.messages.create(:from => '+15109240308',:to => number_to_send_to,:body =>"Security Code is kk")
+
 end
 
 end
